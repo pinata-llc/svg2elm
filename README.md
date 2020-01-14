11f454d3c04f5747b326759e6edc641750584dac
Agustín Zubiaga <sup@aguz.me>
2020-01-14 12:32:51 -0300

    Svg2Elm implementation

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # svg2elm
+
 Turn SVG files into Elm code
+
+Run:
+
+```sh
+$ elm2svg --module Company.Icons chevron.svg
+```
+
+Get:
+
+```elm
+module Company.Icons exposing (..)
+
+import Svg
+import VirtualDom exposing (Attribute, attribute)
+
+chevron : List (Attribute msg) -> Svg.Svg msg
+chevron attrs = Svg.node """svg""" (attrs ++ [attribute """role""" """img""", attribute """xmlns""" """http://www.w3.org/2000/svg""", attribute """viewBox""" """0 0 448 512"""]) [ Svg.node """path"""( [attribute """d""" """M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"""]) []]
+```
---
+++ src/Main.elm
@@ -0,0 +1,158 @@
+module Main exposing (main)
+
+import Cli.Option as Option
+import Cli.OptionsParser as OptionsParser
+import Cli.Program as Program
+import Ports
+import Regex
+import String.Case exposing (toCamelCaseLower)
+import SvgParser exposing (SvgAttribute, SvgNode(..), parseToNode)
+
+
+
+-- Generator
+
+
+quote : String -> String
+quote val =
+    "\"\"\"" ++ val ++ "\"\"\""
+
+
+compileAttributes : List SvgAttribute -> String
+compileAttributes =
+    String.join ", "
+        << List.map
+            (\( name, val ) -> "attribute " ++ quote name ++ " " ++ quote val)
+
+
+compileChildren : List SvgNode -> String
+compileChildren =
+    let
+        nodeJoin node =
+            case node of
+                SvgComment _ ->
+                    " "
+
+                _ ->
+                    ", "
+    in
+    List.foldl (\node b -> b ++ nodeJoin node ++ compileNode False node) ""
+        >> String.dropLeft 1
+
+
+compileNode : Bool -> SvgNode -> String
+compileNode attrs node =
+    case node of
+        SvgElement { name, attributes, children } ->
+            "Svg.node "
+                ++ quote name
+                ++ (if attrs then
+                        " (attrs ++"
+
+                    else
+                        "("
+                   )
+                ++ " ["
+                ++ compileAttributes attributes
+                ++ "]) "
+                ++ "["
+                ++ compileChildren children
+                ++ "]"
+
+        SvgText text ->
+            "Svg.text(" ++ quote text ++ ")"
+
+        SvgComment comment ->
+            "{-" ++ comment ++ "-}"
+
+
+compileFunction : String -> String -> Result String String
+compileFunction name code =
+    let
+        fnName =
+            toCamelCaseLower name
+
+        fixedCode =
+            case Regex.fromString "([\\s\\S]*)<svg" of
+                Nothing ->
+                    code
+
+                Just regex ->
+                    Regex.replaceAtMost 1 regex (\_ -> "<svg") code
+    in
+    parseToNode fixedCode
+        |> Result.map
+            (compileNode True >> (++) (fnName ++ " : List (Attribute msg) -> Svg.Svg msg\n" ++ fnName ++ " attrs = "))
+
+
+
+-- CLI
+
+
+type alias CompileOptions =
+    { moduleName : String
+    , paths : List String
+    }
+
+
+type Msg
+    = FilesRead (List ( String, String ))
+
+
+type alias Model =
+    ()
+
+
+update : CompileOptions -> Msg -> Model -> ( Model, Cmd Msg )
+update options msg _ =
+    case msg of
+        FilesRead files ->
+            let
+                header =
+                    Ok <| "module " ++ options.moduleName ++ " exposing (..)" ++ "\n\nimport Svg\nimport VirtualDom exposing (Attribute, attribute)"
+
+                compiled =
+                    List.foldl (\( name, code ) -> Result.andThen (\r -> Result.map ((++) <| r ++ "\n\n") (compileFunction name code))) header files
+            in
+            case compiled of
+                Ok compiledCode ->
+                    ( (), Ports.print compiledCode )
+
+                Err err ->
+                    ( (), Ports.print <| "Error while parsing SVG: " ++ err )
+
+
+init : Flags -> CompileOptions -> ( Model, Cmd Msg )
+init _ options =
+    ( (), Ports.readFiles options.paths )
+
+
+type alias Flags =
+    Program.FlagsIncludingArgv {}
+
+
+subscriptions : a -> Sub Msg
+subscriptions _ =
+    Ports.filesRead FilesRead
+
+
+programConfig : Program.Config CompileOptions
+programConfig =
+    Program.config
+        |> Program.add
+            (OptionsParser.build CompileOptions
+                |> OptionsParser.with (Option.requiredKeywordArg "module")
+                |> OptionsParser.withRestArgs (Option.restArgs "paths")
+            )
+
+
+main : Program.StatefulProgram Model Msg CompileOptions {}
+main =
+    Program.stateful
+        { printAndExitFailure = Ports.printAndExitFailure
+        , printAndExitSuccess = Ports.printAndExitSuccess
+        , init = init
+        , config = programConfig
+        , subscriptions = subscriptions
+        , update = update
+        }
