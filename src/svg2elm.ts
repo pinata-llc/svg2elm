79259a362b7c35fe4d2c99c5683a874395fa1df3
Agustín Zubiaga <sup@aguz.me>
2020-01-25 18:13:22 -0300

    Add .prettierrc with tabWidth: 4

--- src/svg2elm.ts
+++ src/svg2elm.ts
@@ -2,19 +2,26 @@ const { Elm } = require("./Main.elm");
 const fs = require("fs");
 const path = require("path");
 
-const app = Elm.Main.init({ flags: { argv: process.argv, versionMessage: "0.1.0" } });
+const app = Elm.Main.init({
+    flags: { argv: process.argv, versionMessage: "0.1.0" }
+});
 
 app.ports.print.subscribe((message: string) => console.log(message));
 app.ports.printAndExitFailure.subscribe((message: string) => {
-  console.log(message);
-  process.exit(1);
+    console.log(message);
+    process.exit(1);
 });
 
 app.ports.printAndExitSuccess.subscribe((message: string) => {
-  console.log(message);
-  process.exit(0);
+    console.log(message);
+    process.exit(0);
 });
 
 app.ports.readFiles.subscribe((paths: string[]) => {
-  app.ports.filesRead.send(paths.map(p => [path.basename(p).split(".")[0], fs.readFileSync(p).toString()]));
+    app.ports.filesRead.send(
+        paths.map(p => [
+            path.basename(p).split(".")[0],
+            fs.readFileSync(p).toString()
+        ])
+    );
 });
